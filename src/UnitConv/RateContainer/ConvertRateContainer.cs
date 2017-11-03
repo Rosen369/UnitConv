6d8c8877afc7fec8634e5f09a6d8f78431124678
rosen <rosen369@outlook.com>
2017-11-03 15:03:18 +0800

    add length rate container

---
+++ src/UnitConv/RateContainer/ConvertRateContainer.cs
@@ -0,0 +1,29 @@
+﻿namespace UnitConv
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    internal partial class ConvertRateContainer
+    {
+        static ConvertRateContainer()
+        {
+            _instance = new ConvertRateContainer();
+        }
+
+        private static ConvertRateContainer _instance;
+
+        private ConvertRateContainer()
+        {
+            this.InitLengthRate();
+        }
+
+        public static float GetRate(LengthType fromType, LengthType toType)
+        {
+            var value = 1f;
+            var hasRate = _instance.LengthRateDic.TryGetValue((fromType, toType), out value);
+            if (!hasRate) throw new Exception("not supported to convert");
+            return value;
+        }
+    }
+}
---
+++ src/UnitConv/RateContainer/LengthRate.cs
@@ -0,0 +1,17 @@
+﻿namespace UnitConv
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    internal partial class ConvertRateContainer
+    {
+        private IDictionary<ValueTuple<LengthType, LengthType>, float> LengthRateDic { get; set; }
+
+        private void InitLengthRate()
+        {
+            LengthRateDic = new Dictionary<ValueTuple<LengthType, LengthType>, float>();
+            LengthRateDic.Add((LengthType.Meter, LengthType.Mile), 1.1f);
+        }
+    }
+}
