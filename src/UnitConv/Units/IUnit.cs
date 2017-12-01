4e4b7fc55d2ba1d2407dd6619992c167119832d3
rosen <rosen369@outlook.com>
2017-12-01 17:10:12 +0800

    add unit base and interface

---
+++ src/UnitConv/Units/IUnit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitConv.Units
+{
+    interface IUnit
+    {
+        decimal GetRate();
+
+        string GetDisplay(Languages language);
+    }
+}
---
+++ src/UnitConv/Units/Unit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitConv.Units
+{
+    internal abstract class Unit : IUnit
+    {
+        private Unit()
+        {
+            this.InitRate();
+            this.InitDisplay();
+        }
+
+        protected readonly decimal _rate;
+
+        protected readonly IDictionary<Languages, string> _display;
+
+        protected abstract void InitDisplay();
+
+        protected abstract void InitRate();
+
+        public string GetDisplay(Languages language)
+        {
+            if (_display == null) throw new Exception("display has not been initialized");
+            if (!_display.ContainsKey(language)) throw new Exception("dont have display of language" + language.ToString());
+            return _display[language];
+        }
+
+        public decimal GetRate()
+        {
+            if (_rate == 0) throw new Exception("rate has not been initialized");
+            return _rate;
+        }
+    }
+}
