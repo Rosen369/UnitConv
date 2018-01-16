fa9b2c8de6fd345860381d3e31d36818156c0839
rosen <rosen369@outlook.com>
2018-01-16 15:01:22 +0800

    init volume units

---
+++ src/UnitConv/Units/VolumeUnit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitConv.Units;
+
+namespace UnitConv
+{
+    public abstract class VolumeUnit : Unit
+    {
+    }
+}
---
+++ src/UnitConv/Units/VolumeUnits/CubicMeter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitConv.Units.VolumeUnits
+{
+    public class CubicMeter : VolumeUnit
+    {
+        private CubicMeter()
+        {
+
+        }
+
+        private static readonly CubicMeter _instance = new CubicMeter();
+
+        public static CubicMeter GetInstance()
+        {
+            return _instance;
+        }
+
+        protected override void InitDisplay()
+        {
+            _display = new Dictionary<Languages, string>
+            {
+                { Languages.Chinese, "立方米" },
+                { Languages.English, "m³" }
+            };
+        }
+
+        protected override void InitRate()
+        {
+            _rate = 1m;
+        }
+    }
+}
