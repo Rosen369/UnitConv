6a56d24beae465fc4423321aa43c213acca198b5
rosen <rosen369@outlook.com>
2018-02-08 17:19:28 +0800

    add weight unit

---
+++ src/UnitConv/Units/WeightUnit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitConv.Units;
+
+namespace UnitConv
+{
+    public abstract class WeightUnit : Unit
+    {
+        public static WeightUnit Kilogram
+        {
+            get
+            {
+                return Units.WeightUnits.Kilogram.GetInstance();
+            }
+        }
+    }
+}
---
+++ src/UnitConv/Units/WeightUnits/Kilogram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitConv.Units.WeightUnits
+{
+    public class Kilogram : WeightUnit
+    {
+        private Kilogram()
+        {
+
+        }
+
+        private static readonly Kilogram _instance = new Kilogram();
+
+        public static Kilogram GetInstance()
+        {
+            return _instance;
+        }
+
+        protected override void InitDisplay()
+        {
+            _display = new Dictionary<Languages, string>
+            {
+                { Languages.Chinese, "千克" },
+                { Languages.English, "kg" }
+            };
+        }
+
+        protected override void InitRate()
+        {
+            _rate = 1m;
+        }
+    }
+}
