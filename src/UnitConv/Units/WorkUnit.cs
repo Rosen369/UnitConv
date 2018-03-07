f3522a55b9e05125ffe3af7d575c3a42ede74a52
rosen <rosen369@outlook.com>
2018-03-07 17:09:39 +0800

    add work unit

---
+++ src/UnitConv/Units/WorkUnit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitConv.Units;
+
+namespace UnitConv
+{
+    public abstract class WorkUnit : Unit
+    {
+        public static WorkUnit Joule
+        {
+            get
+            {
+                return Units.WorkUnits.Joule.GetInstance();
+            }
+        }
+    }
+}
---
+++ src/UnitConv/Units/WorkUnits/Joule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitConv.Units.WorkUnits
+{
+    public class Joule : WorkUnit
+    {
+        private Joule()
+        {
+
+        }
+
+        private static readonly Joule _instance = new Joule();
+
+        public static Joule GetInstance()
+        {
+            return _instance;
+        }
+
+        protected override void InitDisplay()
+        {
+            _display = new Dictionary<Languages, string>
+            {
+                { Languages.Chinese, "焦耳" },
+                { Languages.English, "J" }
+            };
+        }
+
+        protected override void InitRate()
+        {
+            _rate = 1m;
+        }
+    }
+}
